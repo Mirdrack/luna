a45cb672acba8092f37e9ad1371439c1f929d5e2
Clemente Estrada <mirdrack@gmail.com>
2017-05-27 16:56:06 -0500

    Added scaffolding for profile and support modules

---
+++ src/components/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-md-4 offset-md-4">
+        <b-card header="Luna App"
+                class="mb-2"
+                title="Hello I'm Luna"
+                show-footer
+        >
+          This is going to be the user profile
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile'
+}
+</script>
---
+++ src/components/Support.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="row">
+      <div class="col-md-4 offset-md-4">
+        <b-card header="Luna App"
+                class="mb-2"
+                title="This is the support module"
+                show-footer
+        >
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'support'
+}
+</script>
